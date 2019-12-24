d8b3fdcb7cc28398b2e90908b425155e580a35fa
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-24 12:27:37 +0530

    Refactored: Replaced .assertEqual() with .assertTrue/.assertFalse

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,56 +6,48 @@ public class UserRegistrationTest {
     @Test
     public void whenGivenFirstName_ifOnlyFirstLetterCapital_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Bruce");
-        Assert.assertEquals(true, valid);
+        Assert.assertTrue(userRegistration.isValidName("Bruce"));
     }
 
     @Test
     public void whenGivenFirstName_ifFirstLetterSmall_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("bruce");
-        Assert.assertEquals(false, valid);
+        Assert.assertFalse(userRegistration.isValidName("bruce"));
     }
 
     @Test
     public void whenGivenFirstName_ifMoreThen3Character_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Bruce");
-        Assert.assertEquals(true, valid);
+        Assert.assertTrue(userRegistration.isValidName("Bruce"));
     }
 
     @Test
     public void whenGivenFirstName_ifLessThen3Character_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Br");
-        Assert.assertEquals(false, valid);
+        Assert.assertFalse(userRegistration.isValidName("Br"));
     }
 
     @Test
     public void whenGivenLastName_ifFirstLetterCapital_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Wayne");
-        Assert.assertEquals(true, valid);
+        Assert.assertTrue(userRegistration.isValidName("Wayne"));
     }
 
     @Test
     public void whenGivenLastName_ifFirstLetterSmall_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("wayne");
-        Assert.assertEquals(false, valid);
+        Assert.assertFalse(userRegistration.isValidName("wayne"));
     }
 
     @Test
     public void whenGivenLastName_ifMoreThen3Character_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Wayne");
-        Assert.assertEquals(true, valid);
+        Assert.assertTrue(userRegistration.isValidName("Wayne"));
     }
 
     @Test
     public void whenGivenLastName_ifLessThen3Character_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValidName("Wa");
-        Assert.assertEquals(false, valid);
+        Assert.assertFalse(userRegistration.isValidName("Wa"));
     }
 }
