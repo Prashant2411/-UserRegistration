c74ed96448f4e5eb34b3549173aee7f57ef83948
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-24 12:18:27 +0530

    Refactored: Changed method name from isValid to isValidName

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean isValid(String name) {
+    public boolean isValidName(String name) {
         String regexPattern = "^[A-Z]{1}[a-z]{2,}";
         return name.matches(regexPattern);
     }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,56 +6,56 @@ public class UserRegistrationTest {
     @Test
     public void whenGivenFirstName_ifOnlyFirstLetterCapital_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Bruce");
+        boolean valid = userRegistration.isValidName("Bruce");
         Assert.assertEquals(true, valid);
     }
 
     @Test
     public void whenGivenFirstName_ifFirstLetterSmall_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("bruce");
+        boolean valid = userRegistration.isValidName("bruce");
         Assert.assertEquals(false, valid);
     }
 
     @Test
     public void whenGivenFirstName_ifMoreThen3Character_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Bruce");
+        boolean valid = userRegistration.isValidName("Bruce");
         Assert.assertEquals(true, valid);
     }
 
     @Test
     public void whenGivenFirstName_ifLessThen3Character_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Br");
+        boolean valid = userRegistration.isValidName("Br");
         Assert.assertEquals(false, valid);
     }
 
     @Test
     public void whenGivenLastName_ifFirstLetterCapital_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Wayne");
+        boolean valid = userRegistration.isValidName("Wayne");
         Assert.assertEquals(true, valid);
     }
 
     @Test
     public void whenGivenLastName_ifFirstLetterSmall_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("wayne");
+        boolean valid = userRegistration.isValidName("wayne");
         Assert.assertEquals(false, valid);
     }
 
     @Test
     public void whenGivenLastName_ifMoreThen3Character_shouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Wayne");
+        boolean valid = userRegistration.isValidName("Wayne");
         Assert.assertEquals(true, valid);
     }
 
     @Test
     public void whenGivenLastName_ifLessThen3Character_shouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean valid = userRegistration.isValid("Wa");
+        boolean valid = userRegistration.isValidName("Wa");
         Assert.assertEquals(false, valid);
     }
 }
